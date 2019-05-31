37fc7e3337641be5df906be8146571aba3f00a6d
Anggit Nendyo <43041396+M3116075@users.noreply.github.com>
2019-05-31 20:56:19 +0700

    update

--- app/src/main/java/d3ti/uns/anggit/caritourguide/HomeActivity.java
+++ app/src/main/java/d3ti/uns/anggit/caritourguide/HomeActivity.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import d3ti.uns.anggit.caritourguide.adapter.HomeViewPagerAdapter;
 
 
 public class HomeActivity extends AppCompatActivity {
-
     private ImageView ivLogo;
     private TabLayout tabLayout;
     private ViewPager viewPager;
--- app/src/main/java/d3ti/uns/anggit/caritourguide/LoginWisatawanFragment.java
+++ app/src/main/java/d3ti/uns/anggit/caritourguide/LoginWisatawanFragment.java
@@ -25,7 +25,7 @@ public class LoginWisatawanFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login_wisatawan, container, false);
 
-        
+
 
         return view;
     }
